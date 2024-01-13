f139b92eda9b05d3f12b4f3c2c8a5c828e6f0de0
Diogo Mendes <up202005393@up.pt>
2024-01-13 23:50:15 +0000

    one hot encoder

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import pandas as pd
 from sklearn.metrics import confusion_matrix
 
 # Load the data from the CSV file
